6f3712b3d7806e3fef3b94d210390c208682e362
Timothy7310 <penlist127@gmail.com>
2023-08-30 17:45:59 +0300

    refactor: remove return in GamesService query

--- src/app/services/GamesService.ts
+++ src/app/services/GamesService.ts
@@ -1,16 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { GamesList, Game, SearchParams, SearchParamsType } from 'app/types';
+import { GamesList, Game, SearchParamsType } from 'app/types';
 
 export const gamesAPI = createApi({
   reducerPath: 'gamesAPI',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://www.freetogame.com/api' }),
   endpoints: (build) => ({
     fetchAllGames: build.query<GamesList, ''>({
-      query: () => {
-        return {
-          url: '/games',
-        };
-      },
+      query: () => ({
+        url: '/games',
+      }),
     }),
     fetchFilteredGames: build.query<GamesList, SearchParamsType>({
       query: (searchParams: SearchParamsType) => {
@@ -25,15 +23,13 @@ export const gamesAPI = createApi({
       },
     }),
     fetchGamesWithQuery: build.query<GamesList, SearchParamsType>({
-      query: (searchParams: SearchParamsType) => {
-        return {
-          url: '/games',
-          params: {
-            platform: searchParams.platform,
-            'sort-by': searchParams['sort-by'],
-          },
-        };
-      },
+      query: (searchParams: SearchParamsType) => ({
+        url: '/games',
+        params: {
+          platform: searchParams.platform,
+          'sort-by': searchParams['sort-by'],
+        },
+      }),
     }),
     fetchGameByID: build.query<Game, string>({
       query: (id: string) => ({
